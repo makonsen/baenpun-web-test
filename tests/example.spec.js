f323eb66b3c1a80128142350d964e1ab4c518f82
makonsen <168194996+makonsen@users.noreply.github.com>
2024-07-12 20:18:00 +0700

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -13,9 +13,6 @@ test('test2', async ({ page }) => {
   await page.goto('https://cloudshop.vclass.in.th/');
   await page.getByRole('link', { name: '' }).click();
   await page.getByPlaceholder('ค้นหา').click();
-  await page.getByPlaceholder('ค้นหา').fill('mflv[');
-  await page.getByPlaceholder('ค้นหา').press('ControlOrMeta+a');
   await page.getByPlaceholder('ค้นหา').fill('ทดสอบ');
   await page.getByRole('button', { name: '' }).click();
-  // await page.goto('https://xn--72c0bbf4i0d4b1a.xn--o3cw4h/');
 });
