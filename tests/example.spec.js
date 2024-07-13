5731695a90098d1f96dc7a02a4e8240f99b95265
makonsen <168194996+makonsen@users.noreply.github.com>
2024-07-13 19:02:10 +0700

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://xn--72c0bbf4i0d4b1a.xn--o3cw4h/');
-  await page.getByRole('link', { name: '' }).click();
-  await page.getByPlaceholder('ค้นหา').click();
-  await page.getByPlaceholder('ค้นหา').fill('ทดสอบ');
-  await page.getByPlaceholder('ค้นหา').press('Enter');
-  await page.locator('#navbarCollapse').getByRole('link', { name: 'หน้าแรก' }).click();
-});
-
-test('test2', async ({ page }) => {
   await page.goto('https://cloudshop.vclass.in.th/');
-  await page.getByRole('link', { name: '' }).click();
-  await page.getByPlaceholder('ค้นหา').click();
-  await page.getByPlaceholder('ค้นหา').fill('ทดสอบ');
-  await page.getByRole('button', { name: '' }).click();
+  await page.getByRole('link', { name: 'เข้าสู่ระบบสำหรับสมาชิก' }).click();
+  await page.getByLabel('Qr Code Image').click();
+  await page.getByLabel('Qr Code Image').fill('นัทธี@คน.ไทย');
+  await page.getByLabel('รหัสผ่าน *').click();
+  await page.getByLabel('รหัสผ่าน *').fill('รหัส123456');
+  await page.getByRole('button', { name: 'เข้าสู่ระบบ', exact: true }).click();
+  await page.getByRole('link', { name: 'จุลสาร' }).click();
+  await page.getByRole('link', { name: 'อ่านเพิ่มเติม ' }).first().click();
+  await page.locator('#navbarCollapse').getByRole('link', { name: 'หน้าแรก' }).click();
+  await page.getByRole('link', { name: '' }).click();
+  await page.getByRole('link', { name: '' }).click();
+  await page.getByRole('link', { name: '' }).click();
+  await page.locator('#topbarCollapse').getByRole('link', { name: 'การส่งสินค้า' }).click();
+  await page.getByRole('link', { name: 'หน้าแรก' }).click();
 });
