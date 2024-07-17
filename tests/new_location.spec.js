5c04678aba058d99ef1433a0716a1ecf095991eb
makonsen <168194996+makonsen@users.noreply.github.com>
2024-07-17 19:51:36 +0700

    Update new_location.spec.js

--- tests/new_location.spec.js
+++ tests/new_location.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-
+// const i = 3;
 test('Testing the address adding system', async ({ page }) => {
     await page.goto('https://cloudshop.vclass.in.th/');
     await page.getByRole('link', { name: 'เข้าสู่ระบบสำหรับสมาชิก' }).click();
@@ -10,22 +10,23 @@ test('Testing the address adding system', async ({ page }) => {
     await page.getByRole('button', { name: 'เข้าสู่ระบบ', exact: true }).click();
     await page.getByRole('link', { name: '' }).click();
     await page.getByRole('link', { name: ' ที่อยู่ ' }).click();
-    await page.getByRole('link', { name: 'เพิ่มที่อยู่ใหม่ ' }).click();
-    await page.getByLabel('ชื่อที่อยู่').click();
-    await page.getByLabel('ชื่อที่อยู่').fill('1');
-    await page.getByLabel('ชื่อ-นามสกุล').click();
-    await page.getByLabel('ชื่อ-นามสกุล').fill('นัทธี มกรเสน');
-    await page.getByLabel('เบอร์โทรศัพท์').click();
-    await page.getByLabel('เบอร์โทรศัพท์').fill('0123456789');
-    await page.getByLabel('ที่อยู่', { exact: true }).click();
-    await page.getByLabel('ที่อยู่', { exact: true }).fill('1');
-    await page.getByPlaceholder('ค้นหาจังหวัด').click();
-    await page.locator('div').filter({ hasText: /^ตาก \/ Tak$/ }).first().click();
-    await page.getByPlaceholder('ค้นหาเขต/อำเภอ').click();
-    await page.getByText('พบพระ / Phop Phra').click();
-    await page.getByPlaceholder('ค้นหาแขวง/ตำบล หรือ เลขไปรษณีย์').click();
-    await page.getByText('ช่องแคบ / Chong Khaep -').click();
-    await page.getByRole('button', { name: 'บันทึก' }).click();
-    await page.getByRole('link', { name: ' ที่อยู่ ' }).click();
-    await expect(page).toHaveTitle(/แบ่งปั๋น/);
+    for (let i = 0; i <= 4 ; i++) {
+        await page.getByRole('link', { name: 'เพิ่มที่อยู่ใหม่ ' }).click();
+        await page.getByLabel('ชื่อที่อยู่').click();
+        await page.getByLabel('ชื่อที่อยู่').fill(''+i);
+        await page.getByLabel('ชื่อ-นามสกุล').click();
+        await page.getByLabel('ชื่อ-นามสกุล').fill('นัทธี มกรเสน');
+        await page.getByLabel('เบอร์โทรศัพท์').click();
+        await page.getByLabel('เบอร์โทรศัพท์').fill('0123456789');
+        await page.getByLabel('ที่อยู่', { exact: true }).click();
+        await page.getByLabel('ที่อยู่', { exact: true }).fill('1');
+        await page.getByPlaceholder('ค้นหาจังหวัด').click();
+        await page.locator('div').filter({ hasText: /^ตาก \/ Tak$/ }).first().click();
+        await page.getByPlaceholder('ค้นหาเขต/อำเภอ').click();
+        await page.getByText('พบพระ / Phop Phra').click();
+        await page.getByPlaceholder('ค้นหาแขวง/ตำบล หรือ เลขไปรษณีย์').click();
+        await page.getByText('ช่องแคบ / Chong Khaep -').click();
+        await page.getByRole('button', { name: 'บันทึก' }).click();
+        await page.getByRole('link', { name: ' ที่อยู่ ' }).click();
+    }
 });
