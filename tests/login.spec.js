c04aae903cb3067e77f737fd6df4ac936bb5423e
makonsen <168194996+makonsen@users.noreply.github.com>
2024-08-06 19:53:28 +0700

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('login', async ({ page }) => {
   test.setTimeout(1200000);
-  await page.goto('https://cloudshop.vclass.in.th/');
+  await page.goto('http://localhost/');
   await page.getByRole('link', { name: 'เข้าสู่ระบบสำหรับสมาชิก' }).click();
   await page.getByLabel('Qr Code Image').click();
   await page.getByLabel('Qr Code Image').fill('นัทธี@คน.ไทย');
@@ -10,4 +10,3 @@ test('login', async ({ page }) => {
   await page.getByLabel('รหัสผ่าน *').fill('รหัส123456');
   await page.getByRole('button', { name: 'เข้าสู่ระบบ', exact: true }).click();
 });
-
