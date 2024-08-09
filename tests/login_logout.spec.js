7a7d4949f2cbadcc5806e43a2a71a0e8459a850d
makonsen <168194996+makonsen@users.noreply.github.com>
2024-08-09 22:43:34 +0700

    new file

--- tests/login_logout.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost/');
-  await page.getByRole('link', { name: '' }).click();
-  await page.getByRole('button', { name: '' }).click();
-  await page.getByRole('link', { name: 'ผลิตภัณฑ์จากชุมชน' }).first().click();
-  await page.getByRole('link', { name: 'OOlong Tea' }).first().click();
-});
