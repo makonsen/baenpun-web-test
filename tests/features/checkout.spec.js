50f5d14554c5020c5daace95c7f9de49aa4a4244
makonsen <makonsennatthi@gmail.com>
2024-08-26 22:32:43 +0700

    Delete checkout.spec.js

--- tests/features/checkout.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-    await page.goto('http://localhost/');
-    await expect(page.getByRole('link', { name: 'บัญชีของฉัน (มกรเสน' })).toHaveText('ข้อความที่คุณคาดหวัง');
-    await page.getByRole('link', { name: 'เข้าสู่ระบบสำหรับสมาชิก' }).click();
-});
