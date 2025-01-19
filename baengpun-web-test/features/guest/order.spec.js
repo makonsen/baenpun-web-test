7ada90a6d6511695a1c460414aa754e46ceda469
makonsen <makonsennatthi@gmail.com>
2025-01-19 21:07:57 +0700

    Create order.spec.js

---
+++ baengpun-web-test/features/guest/order.spec.js
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+const serverUrl = process.env.SERVER_URL || 'http://localhost';
+const Dates = (new Date()).getTime();
+const username = 'user_' + Dates;
+const email = 'makonsennatthi_' + Dates + '@gmail.com';
+test('Order as a guest', async ({ page }) => {
+    await page.goto('https://cloudshop.vclass.in.th/product/64');
+    // await page.getByPlaceholder('Please tell us what kind of').click();
+    // await page.getByPlaceholder('Please tell us what kind of').fill('หมายเหตุ');
+    await page.getByRole('button', { name: 'ใส่รถเข็น ' }).click();
+    await page.getByText('เลือกสินค้า', { exact: true }).click();
+    await page.getByRole('button', { name: 'ดำเนินการต่อ' }).click();
+    await page.getByRole('button', { name: 'ชำระเงินโดยไม่เป็นสมาชิก' }).click();
+    await page.getByPlaceholder('ชื่อ').fill(username);
+    await page.getByPlaceholder('อีเมล', { exact: true }).fill(email);
+    await page.getByPlaceholder('เบอร์โทรศัพท์').fill('0123456789');
+    await page.getByPlaceholder('ที่อยู่').fill('ที่อยู่');
+    await page.getByPlaceholder('ค้นหาจังหวัด').click();
+    await page.getByText('กรุงเทพมหานคร / Bangkok').click();
+    await page.getByPlaceholder('ค้นหาเขต/อำเภอ').click();
+    await page.getByText('เขตคลองสาน / Khet Khlong San').click();
+    await page.getByPlaceholder('ค้นหาแขวง/ตำบล หรือ เลขไปรษณีย์').click();
+    await page.getByText('คลองต้นไทร / Khlong Ton Sai -').click();
+    await page.getByRole('button', { name: 'บันทึกที่อยู่' }).click();
+    await page.getByRole('button', { name: 'Ok' }).click();
+    await page.getByRole('button', { name: 'ดำเนินการต่อ' }).click();
+    await page.getByRole('link', { name: 'ดูรายละเอียดคำสั่งซื้อ' }).click();
+});
